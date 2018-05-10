c98379ac44aba02425a24d3ec13be280fd520afd
Flavius <bancflavius@gmail.com>
2018-05-10 23:29:51 +0300

    teme0.4

--- src/TemaNr21.java
+++ src/TemaNr21.java
@@ -1,15 +1,24 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TemaNr21 {
     public static void main(String[] args) {
 
-        int sir[] = {55, 23,11, 14,21, 22, 24, 0, -6, 11, 56, 67, 11 , 4,};
+        int sir[] = new int[15];
         int sir2[] = new int[sir.length];
         int counter = 0;
-        int min = 21;
-        int max = 56;
+        int min;
+        int max;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Introduceti valoarea minima: ");
+        min = input.nextInt();
+        System.out.print("Introduceti valoarea maxima: ");
+        max = input.nextInt();
 
         for(int i = 0; i < sir.length; i++) {
+            int x =(int)(Math.random() * 100 + 1);
+            sir[i] = x;
             if ( (sir[i] >= min) && (sir[i] <= max) ) {
                 sir2[counter] = sir[i];
                 counter++;
@@ -17,7 +26,6 @@ public class TemaNr21 {
         }
         int[] sirNou = Arrays.copyOfRange(sir2, 0, counter);
 
-
-        System.out.println(Arrays.toString(sirNou));
+        System.out.println("Sirul cu valorile cuprinse intre " + min + " si " + max + " din sirul generat este: " + Arrays.toString(sirNou));
     }
 }
